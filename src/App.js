00531ca794b22b272fbfae1bf9207b1a00cd74d8
MatiasG91 <97643198+MatiasG91@users.noreply.github.com>
2022-01-20 10:57:20 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Navbar from './Components/NavBar'
+import productos from './Components/Productos'
+import contacto from './Components/Contacto'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola lumi bella!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Navbar/>
+        <switch>
+          <route path= '/productos' exact component= {productos}/>
+          <route path= '/contacto' exact component= {contacto}/>
+          
+
+        </switch>
+      </Router>
+      </div>
   );
 }
 
---
+++ src/Components/Productos.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const productos = () => {
+    return (
+        <div>
+            <h1>
+                Esta es la pagina de Producto
+            </h1>
+        </div>
+    ) }
+
+    export default productos
